dd4fdf3e984249a9bb7df0d87d8d74a0b4a84239
Brian Guo <kuobrian614@gmail.com>
2020-10-27 15:01:42 +0800

    detect from video or camera

--- demo.py
+++ demo.py
@@ -40,9 +40,6 @@ def DemoSingleImage(model, img_path, img_tfs, device='cuda'):
     transform_img = torch.unsqueeze(img_tfs(img_rgb), 0)
     transform_img = transform_img.to(device, dtype=torch.float)
 
-    print(transform_img.shape)
-
-
     pred_raw = model(transform_img)
     pred_raw = pred_raw.data.cpu().numpy()
     pred_label_imgs = np.argmax(pred_raw, axis=1)
@@ -50,15 +47,9 @@ def DemoSingleImage(model, img_path, img_tfs, device='cuda'):
         if label.id in np.unique(pred_label_imgs):
             print(label.trainId, " => ", label.name)
 
-
     pred_label_imgs = pred_label_imgs.astype(np.uint8)[0]
     pred_label_img_color = draw_label(pred_label_imgs)
-
-
-    print(pred_label_img_color.shape)
     resize_vis = cv2.resize(img, (params.img_w, params.img_h))
-
-
     overlayed_img = 0.35*resize_vis + 0.65*pred_label_img_color
     overlayed_img = overlayed_img.astype(np.uint8)
 
@@ -70,6 +61,43 @@ def DemoSingleImage(model, img_path, img_tfs, device='cuda'):
     cv2.imshow("combined_img", combined_img)
     cv2.waitKey(0)
     
+
+
+def DemoVideo(model, video_path, img_tfs, source='video', device='cuda'):
+    save_path = './DemoVideo.avi'
+    out = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*"MJPG"), 30, (2*params.img_w, 2*params.img_h))
+    if source == 'video':
+        cam = cv2.VideoCapture(video_path)
+    elif source == 'camera':
+        cam = cv2.VideoCapture(0)
+    while True:
+        ret, img = cam.read()
+        if ret == True:
+            img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            img_rgb = img_rgb / 255.0
+            img_rgb = img_rgb.astype(np.float32)
+            transform_img = torch.unsqueeze(img_tfs(img_rgb), 0)
+            transform_img = transform_img.to(device, dtype=torch.float)
+            pred_raw = model(transform_img)
+            pred_raw = pred_raw.data.cpu().numpy()
+            pred_label_imgs = np.argmax(pred_raw, axis=1)
+            pred_label_imgs = pred_label_imgs.astype(np.uint8)[0]
+            pred_label_img_color = draw_label(pred_label_imgs)
+            resize_vis = cv2.resize(img, (params.img_w, params.img_h))
+            overlayed_img = 0.35*resize_vis + 0.65*pred_label_img_color
+            overlayed_img = overlayed_img.astype(np.uint8)
+
+            combined_img = np.zeros((2*params.img_h, 2*params.img_w, 3), dtype=np.uint8)
+            combined_img[0:params.img_h, 0:params.img_w] = resize_vis
+            combined_img[0:params.img_h, params.img_w:(2*params.img_w)] = pred_label_img_color
+            combined_img[params.img_h:(2*params.img_h), (int(params.img_w/2)):(params.img_w + int(params.img_w/2))] = overlayed_img
+            out.write(combined_img)
+            cv2.imshow("combined_img", combined_img)
+            cv2.waitKey(1)
+        else:
+            break
+    cam.release()
+
 
 
 
@@ -103,21 +131,10 @@ if __name__ == "__main__":
     model.to(device)
     model.eval()
 
-    DemoSingleImage(model, img_path='./testimg.jpg', img_tfs=img_tfs)
+    # DemoSingleImage(model, img_path='./testimg.jpg', img_tfs=img_tfs)
     
-
-    # video_paths = [
-    #             "./exp/Test3.mp4",
-    #             "./exp/20200806_experiment1.avi",
-    #             "./exp/20200806_experiment2.avi"]
-
-    # for video_path in video_paths:
-    #     print("Predict: ", video_path)
-    #     ckpt_name = os.path.splitext(ckpt_path.split("/")[-1])[0]
-    #     save_path = './exp/{}_{}_{}.avi'.format(os.path.splitext(video_path)[0].split('/')[-1], folder, ckpt_name)
-    #     # if not os.path.exists(save_path):
-    #     infer_video(model, video_tfs, device, video_path, save_path)
-    # assert(0)
+    DemoVideo(model, './test.avi', img_tfs=img_tfs, source='camera', device='cuda')
+    
 
 
 
